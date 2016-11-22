839c64c1f49d8d620b1b5daf3a3935feb0678e9f
Kelly Hellinx <kelly.hellinx@digipolis.be>
2016-11-22 15:30:06 +0100

    Added sample api project and base components

---
+++ Digipolis.BusinessLogicDecorated.SampleApi/Logic/Inputs/GetPersonInput.cs
@@ -0,0 +1,14 @@
+﻿using Digipolis.BusinessLogicDecorated.Inputs;
+using Digipolis.BusinessLogicDecorated.SampleApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Digipolis.BusinessLogicDecorated.SampleApi.Logic.Inputs
+{
+    public class GetPersonInput : GetInput<Person>
+    {
+        public string Fields { get; set; }
+    }
+}
---
+++ Digipolis.BusinessLogicDecorated.SampleApi/Logic/Preprocessors/PersonPreprocessor.cs
@@ -0,0 +1,17 @@
+﻿using Digipolis.BusinessLogicDecorated.Preprocessors;
+using Digipolis.BusinessLogicDecorated.SampleApi.Entities;
+using Digipolis.BusinessLogicDecorated.SampleApi.Logic.Inputs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Digipolis.BusinessLogicDecorated.SampleApi.Logic.Preprocessors
+{
+    public class PersonPreprocessor : IGetPreprocessor<Person, GetPersonInput>
+    {
+        public void Preprocess(GetPersonInput input)
+        {
+        }
+    }
+}
