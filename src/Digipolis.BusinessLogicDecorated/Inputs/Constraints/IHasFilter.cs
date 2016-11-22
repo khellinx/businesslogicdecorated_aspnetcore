1a4f2c9f09c6d1b3c894895b462ac6576fc19273
Kelly Hellinx <kelly.hellinx@digipolis.be>
2016-11-22 15:26:08 +0100

    Added input constraint interfaces

---
+++ src/Digipolis.BusinessLogicDecorated/Inputs/Constraints/IHasFilter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Digipolis.BusinessLogicDecorated.Inputs.Constraints
+{
+    public interface IHasFilter<TEntity>
+    {
+        Func<IQueryable<TEntity>, IQueryable<TEntity>> Filter { get; set; }
+    }
+}
---
+++ src/Digipolis.BusinessLogicDecorated/Inputs/Constraints/IHasIncludes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Digipolis.BusinessLogicDecorated.Inputs.Constraints
+{
+    public interface IHasIncludes<TEntity>
+    {
+        Func<IQueryable<TEntity>, IQueryable<TEntity>> Includes { get; set; }
+    }
+}
